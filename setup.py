1c78a903164a11314e1d000585c843999afe8735
Ilana Z Rubin <ilana.z.rubin@columbia.edu>
2018-05-04 13:59:54 -0400

    completing seqlib package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="seqlib",
+    version="0.1",
+    packages=find_packages(),
+    author="Ilana Z Rubin",
+    description="A package for simple numpy functions on sequence arrays",
+    classifiers=["Programming Language :: Python :: 3"],
+)
